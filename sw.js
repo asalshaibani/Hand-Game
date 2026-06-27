f97c68458c49b222435f9e3bf2890bcaf63dae58
asalshaibani <e_wap@hotmail.com>
2026-06-28 03:15:59 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'hand-v1';
+const FILES = [
+  './hand_game_calculator.html',
+  './manifest.json',
+  './icon.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
